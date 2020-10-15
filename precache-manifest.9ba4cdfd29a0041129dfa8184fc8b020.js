7e4245a314017baf5d189bfe392aff8e9846da3b
Kushal5466 <40416862+khushalreddy@users.noreply.github.com>
2020-10-15 20:15:52 +0530

    Updates

---
+++ precache-manifest.9ba4cdfd29a0041129dfa8184fc8b020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbb03306768d01e3c90410952139759",
+    "url": "/React_App/index.html"
+  },
+  {
+    "revision": "681010c0dd39f75871b0",
+    "url": "/React_App/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "d5d42d5dd8e3a27d6747",
+    "url": "/React_App/static/js/2.b89c44ff.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React_App/static/js/2.b89c44ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681010c0dd39f75871b0",
+    "url": "/React_App/static/js/main.741a6cb8.chunk.js"
+  },
+  {
+    "revision": "bc10f16d10cbc1b594a8",
+    "url": "/React_App/static/js/runtime-main.a44f7430.js"
+  }
+]);
